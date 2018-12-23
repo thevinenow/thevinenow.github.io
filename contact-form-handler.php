65873f9ca92880e76c4b125514acb6c997277cae
thevinenow <33191921+thevinenow@users.noreply.github.com>
2018-12-22 18:54:42 -0800

    contact

---
+++ contact-form-handler.php
@@ -0,0 +1,25 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['Email'];
+    $message = $_POST['message'];
+
+    $email_from = 'http://www.thevinenow.com/';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                   "User Email: $visitor_email.\n".
+                      "User Message: $message_email.\n";
+
+
+    $to = "kaykyungahwoo@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject, $email_body, $headers);
+
+    heater("Location: index.html");
+
+ ?>
