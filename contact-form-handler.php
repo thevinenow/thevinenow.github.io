cae403e3fd8b861ee8cc13eede4539a43d79ca9a
thevinenow <33191921+thevinenow@users.noreply.github.com>
2019-11-30 17:15:41 -0800

    contact

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,25 +1,51 @@
 <?php
+$errors = '';
+$myemail = 'kaykyungahwoo@gmail.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
 
-    $name = $_POST['name'];
-    $visitor_email = $_POST['Email'];
-    $message = $_POST['message'];
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
 
-    $email_from = 'http://www.thevinenow.com/';
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
 
-    $email_subject = "New Form Submission";
+if( empty($errors))
+{
+	$to = $myemail;
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message";
 
-    $email_body = "User Name: $name.\n".
-                   "User Email: $visitor_email.\n".
-                      "User Message: $message_email.\n";
+	$headers = "From: $myemail\n";
+	$headers .= "Reply-To: $email_address";
 
-    $to = "kaykyungahwoo@gmail.com";
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact-form-thank-you.html');
+}
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
 
-    $headers = "From: $email_from \r\n";
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
 
-    $headers .= "Reply-To: $visitor_email \r\n";
 
-    mail($to,$email_subject, $email_body, $headers);
-
-    heater("Location: index.html");
-
- ?>
+</body>
+</html>
